8fd91cf2a322a0760dadd2118b9cc5825f13ad7a
Stayss <stacey.mevel@gmail.com>
2023-02-01 11:45:14 +0100

    Mise en place des dossiers et fichiers pour ce mini projet, puis mise en place des images, des flèches directionnelles et du css de base. Reste à faire : La logique pour afficher les différentes images ainsi que la logique inProgress pour évité les spams au click

---
+++ src/Components/Slider/Slider.js
@@ -0,0 +1,36 @@
+import React, { useState} from 'react'
+import'./Slider.css'
+import dataSlider from './dataSlider'
+import BtnSlider from './BtnSlider'
+
+export default function Slider() {
+
+    const [slideAnim, setSlideAnim] = useState( {
+        index: 1,
+        inProgress: false
+    })
+
+    const nextSlide = () => {
+
+    }
+
+    const prevSlide = () => {
+
+    }
+  return (
+    <div className='container-slider'>
+        {dataSlider.map((obj, index) => {
+            return (
+                <div
+                    key={obj.id}
+                    className={slideAnim.index === index + 1 ? "slide active-anim" : "slide"}
+                >   
+                    <img src={process.env.PUBLIC_URL + `/Imgs/img${index + 1}.jpg`} alt="" />
+                </div>
+            )
+        })}
+        <BtnSlider moveSlide={nextSlide} direction={"next"}/>
+        <BtnSlider moveSlide={prevSlide} direction={"previous"}/>
+    </div>
+  )
+}
